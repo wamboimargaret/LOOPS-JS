7ceab6f89f871c5b2277727c15933ab8bfd8c2b2
wamboimargaret <obinyowinfridaa@gmail.com>
2023-04-05 12:46:25 +0300

    loopsjs

---
+++ loops.js
@@ -0,0 +1,30 @@
+//Write a function that accepts an array of strings and console.logs each element using a for loopfu
+function loopArr(arr){
+    for(let i = 0; i < arr.length; i++){
+     console.log(arr[i]);
+    }
+
+}
+let arr = ["Java","Kotlin","Python","JavaScript"];
+loopArr(arr)
+
+
+//Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
+let num = [3,6,9,12,15];
+num.forEach
+
+
+//Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5. Console the array with the new values
+
+//Write a function that takes in the following array and use a while loop to iterate and break when the item is equal to the fourth index
+let arrNum = [1,2,3,4,5,6,7,8,9];
+
+//Write a function that takes in a an array of strings and use a continue statement when the item is at the second index
+let fruits= ['apple','plum','banana','strawberries','kiwi']
+
+
+
+
+
+
+
